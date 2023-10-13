e3440d65393276ccedbec50451bd5bde3f5716a7
Egorov Dmitry <d@t.ru>
2023-10-13 12:05:48 +0500

    first commit

---
+++ measurement/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.TextField()
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
+
+    def __str__(self):
+        return self.name
+
+class Measurement(models.Model):
+    value = models.FloatField()
+    project = models.ForeignKey(Project, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+    updated_at = models.DateTimeField(
+        auto_now=True
+    )
